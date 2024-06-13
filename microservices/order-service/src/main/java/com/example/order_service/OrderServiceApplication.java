a6bf29dc1fefc9f2284f70e127219bfee32f767e
Hatem Boudabra <hatemboudabra41@gmail.com>
2024-06-13 10:13:14 +0200

    commit

--- microservices/order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
+++ microservices/order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
@@ -2,8 +2,8 @@ package com.example.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 @EnableEurekaClient
